8f1d30ca43735274d0c1a23f8360f86e31ac06d2
Hugh Mitton <hm@hughmitton.com>
2016-11-29 14:56:53 +0100

    rough front page working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,9 +1,11 @@
 class PagesController < ApplicationController
+
   def query
     @search_results = Massage.where city: params[:city], category: params[:category]
     return @search_results
   end
 
-  def show
+  def search
+    @massages = Massage.all
   end
 end
