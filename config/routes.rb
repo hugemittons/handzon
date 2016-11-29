a3e83944899aa8dbe429bce0196d136f9f1c4199
Hugh Mitton <hm@hughmitton.com>
2016-11-29 14:26:23 +0100

    routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,6 @@ Rails.application.routes.draw do
   resources :massages
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
 
 end
