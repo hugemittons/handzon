f926469b06a5b5e7bd439d0244c7ad2acda1d32c
matthew goodwin <matthewjgoodwin@gmail.com>
2016-11-29 13:34:00 +0100

    controller added with show, new, create, destroy

---
+++ app/controllers/appointments_controller.rb
@@ -0,0 +1,13 @@
+class AppointmentsController < ApplicationController
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  get 'appointments/show'
+
+  get 'appointments/new'
+
+  get 'appointments/create'
+
+  get 'appointments/destroy'
+
 resources :massages
 devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
