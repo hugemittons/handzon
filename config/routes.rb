c6daf20a6bc7692003968297b74e9ec7bef784f5
Beehane <b.hanae@gmail.com>
2016-11-29 12:40:46 +0100

    Resolving massages branch

--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,6 @@ devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 root to: "massages#index"
 
+resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
+
 end
