4a151dbf6ea5213c326f3edd49291debb6353d74
Margot Sylvain <margot.sylvain@gmail.com>
2016-11-29 16:12:22 +0100

    Commit with User validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
   has_many :appointments
+
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encrypted_password, presence: true
 end
