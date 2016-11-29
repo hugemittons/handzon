52c7d72fcc164f17bb3ef6cf52d6b25860497ccd
matthew goodwin <matthewjgoodwin@gmail.com>
2016-11-29 17:29:54 +0100

    appointment create controller

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,13 +1,37 @@
 class AppointmentsController < ApplicationController
+  before_action :find_appointment, only: [:show]
+  before_action :find_massage, only: [:create]
+  # before_action :find_user, only: [:show]
+  # ^ not needed, device provides access on every page
+
   def show
   end
 
-  def new
-  end
-
   def create
+    # need: datetime, user_id and massage_id
+    @appointment = current_user.appointments.build(appointment_params)
+    # ^ user_id and datetime..
+    # ^^ creates 'appointments.build' and sets the user_id' to 'current_user_id'
+    # ^^^ appointement_params(datetime) are passed to the 'new appointment instance' from the form
+    @appointment.massage = @massage
+    # ^ params is the massage hash with info about massage params ={massage_id: 11, category:...}
+    # ^^ the path requires massage_id
+    # ^^^ path: massage/massage_id/appointments
+    @appointment.save
+    # redirect
   end
 
-  def destroy
+  private
+
+  def appointment_params
+    params.require(:appointment).premit(:datetime)
   end
+
+  def find_massage
+    @massage = Massage.find(params[:massage_id])
+  end
+  # def find_user
+  #   @user = User.find(params[:user_id])
+  # end
+  # ^ above is not used bc we use device/current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,13 @@
 Rails.application.routes.draw do
-  get 'appointments/show'
 
-  get 'appointments/new'
-
-  get 'appointments/create'
-
-  get 'appointments/destroy'
-
-resources :massages
-devise_for :users
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-root to: "massages#index"
+  root to: "massages#index"
 
-resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
-resources :appointments, only: [:new, :create, :show, :destroy]
+  resources :users, only: [:show, :new, :create, :edit, :update, :destroy]
 
+  resources :massages, only: [:show] do
+    resources :appointments, only: [:create]
+  end
+  resources :appointments, only:[:show]
 end
