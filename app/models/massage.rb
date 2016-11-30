bc2308a4d8b530306fffd786a16be7e366bc4e69
Hugh Mitton <hm@hughmitton.com>
2016-11-30 14:50:18 +0100

    cities are now fixed / determined by us

--- app/models/massage.rb
+++ app/models/massage.rb
@@ -1,11 +1,12 @@
 class Massage < ApplicationRecord
-
+  CITIES = ["Barcelona", "San Francisco", "Paris", "London", "Auckland"]
   CATEGORIES = ["Thai", "Berber", "Swedish", "Holistic", "Shiatsu"]
   validates :category, inclusion: { in: CATEGORIES }
   validates :description, presence: true
   validates :tagline, presence: true
   validates :price, presence: true, uniqueness: true
   validates :length, presence: true, uniqueness: true
+  validates :city, inclusion: { in: CITIES }
 
   has_many :attachments
   belongs_to :user
