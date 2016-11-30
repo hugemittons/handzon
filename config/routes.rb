6197d523f0d03ba6bf3845821344be30c65e13e8
matthew goodwin <matthewjgoodwin@gmail.com>
2016-11-30 17:44:22 +0100

    simple form finished but not styled mattg

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,9 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  resources :massages, only: [:show, :index] do
-    resources :appointments, only: [:create]
-  end
+  resources :massages, only: [:show, :index]
+  post "appointments", to: "appointments#create", as: "massage_appointments"
+  # => PATH                   VERB     URI PATTERNS                 CONTROLLER
+  # => massage_appointments   POST    /appointments(.:format)       appointments#create
   resources :appointments, only:[:show]
 end
