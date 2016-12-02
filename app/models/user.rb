df4e54fa57cc1b5a39b6c8569f490ec42eef347c
Margot Sylvain <margot.sylvain@gmail.com>
2016-12-02 15:35:01 +0100

    Delete massage and appointments of users destroyed

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,8 +4,8 @@ class User < ApplicationRecord
   mount_uploader :photo, UserPhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_many :appointments
-  has_many :massages
+  has_many :appointments, dependent: :destroy
+  has_many :massages, dependent: :destroy
 
   validates :name, presence: true
   validates :email, presence: true, uniqueness: true
