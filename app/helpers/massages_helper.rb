7970ba788b4b99e5d3e6c716ef66227e769eea46
Hugh Mitton <hm@hughmitton.com>
2016-12-02 15:41:12 +0100

    designing show page stuff

--- app/helpers/massages_helper.rb
+++ app/helpers/massages_helper.rb
@@ -6,4 +6,20 @@ module MassagesHelper
       cl_image_path @massage.photo, height: 300, width: 400, crop: :fill
     end
   end
+
+
+  def get_avatar
+    if @massage.user.photo.blank?
+      image_tag "http://placehold.it/100x100", class: "card-user"
+    else
+      cl_image_tag @massage.user.photo, height: 100, width: 100, crop: :fill, gravity: :face
+    end
+  end
+
+  def add_review_show
+    if user_signed_in?
+      render 'massages/add_review'
+    else
+    end
+  end
 end
